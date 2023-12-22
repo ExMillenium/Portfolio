4fbad1680e1cb5e10ae888cc3e8197a376a7b6dc
ExMillenium <96273509+ExMillenium@users.noreply.github.com>
2023-12-22 07:48:13 -0400

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,73 @@
+module.exports = {
+  "theme": {
+    "extend": {
+      "colors": {
+        "secondary": {
+          "grey400": "#eff0f2ff",
+          "gray500": "#d9d9d9ff"
+        },
+        "Danger": {
+          "300": "#A22020",
+          "500": "#BF2626",
+          "700": "#E14747"
+        },
+        "Success": {
+          "300": "#199033",
+          "500": "#32A94C",
+          "700": "#4CC366"
+        },
+        "Primary": {
+          "100": "#003EB3",
+          "300": "#0074F0",
+          "500": "#14A9FF",
+          "700": "#85DCFF"
+        },
+        "Gray": {
+          "500": "#595959",
+          "700": "#999999",
+          "800": "#595959",
+          "900": "#D9D9D9",
+          "Black": "#000000",
+          "White": "#FFFFFF"
+        },
+        "primary1": {
+          "blue100": "#291477ff",
+          "blue80": "#5228f5ff",
+          "blue60": "#e2ddf5ff"
+        },
+        "foreground": "#000000",
+        "background": "#FFFFFF"
+      },
+      "spacing": {
+        "FiveUnits": "80px",
+        "Unit": "16px",
+        "OneAndHalfUnits": "24px",
+        "ThreeUnits": "48px",
+        "TwoUnits": "32px",
+        "FourUnits": "64px",
+        "SixUnits": "96px",
+        "HalfUnit": "8px"
+      },
+      "borderRadius": {
+        "Radius8": "8px",
+        "Round": "50%",
+        "Radius4": "4px",
+        "Radius2": "2px"
+      },
+      "scale": {
+        "XXLarge": "288px",
+        "XLarge": "192px",
+        "XSmall": "16px",
+        "MaxWidth": "1400px",
+        "Medium": "96px",
+        "Small": "48px",
+        "Large": "144px"
+      }
+    }
+  },
+  "plugins": [],
+  "content": [
+    "./src/**/*.{html,js,ts,jsx,tsx}",
+    "./*.html"
+  ]
+}
